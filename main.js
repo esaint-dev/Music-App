aecc602413f9db06bf084ab3b1bcdf0b660a4ae0
esaint-dev <78765900+esaint-dev@users.noreply.github.com>
2024-01-03 02:12:31 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const BoxEl = document.getElementById('logReg');
 const Btn = document.getElementById('signUp');
 const contEl = document.getElementById('ovrlay');
+const close = document.getElementById('close')
 
 Btn.addEventListener('click', function() {
     BoxEl.style.display = "block";
     contEl.style.display = "block"
 
 });
+
+close.onclick = function () {
+    BoxEl.style.display = "none"
+    contEl.style.display = "none"
+}
 
